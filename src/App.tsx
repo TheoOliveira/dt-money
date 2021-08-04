70df2e7198ad72d68c0bf16765ed649bc05902bf
Théo <theophilo.oliveira@gmail.com>
2021-08-03 23:33:50 -0300

    summary and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
@@ -10,6 +11,7 @@ export  function App() {
   return (
     <>
       <Header/>
+      <Dashboard/>
       <GlobalStyle/>
     </>
   );
