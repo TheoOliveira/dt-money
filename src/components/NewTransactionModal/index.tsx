ddb099510d8de9015f39cfbb1065eefd1671001c
TheoOliveira <theophilo.oliveira@gmail.com>
2022-01-26 23:35:08 -0300

    styled component button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from "react-modal";
-import { Container, TypeContainer } from "./styles";
+import { Container, TypeContainer, RadioBox } from "./styles";
 import fechar from '../../assets/fechar.svg';
 import incomeImg from '../../assets/entradas.svg';
 import outcomeImg from '../../assets/saida.svg';
+import { useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,6 +15,7 @@ export function NewTransactionmodal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType ] = useState('deposit');
   return (
     <Modal
     overlayClassName="react-modal-overlay"
@@ -28,16 +30,21 @@ export function NewTransactionmodal({
         <input type="text" placeholder="Titulo"></input>
         <input type="number" placeholder="Valor"></input>
         <TypeContainer>
-          <button
+          <RadioBox
             type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
-          <button>
+          </RadioBox>
+          <RadioBox
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+          >
           <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TypeContainer>
         <input type="text" placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
